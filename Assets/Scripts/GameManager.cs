6690719ee963134d7a3276440a18e5a73706263a
vimuzumu <michaelm3@gmail.com>
2020-06-25 18:57:08 +0300

    grows by exp in steps

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private const float EXP_PER_SIZE_MODIFIER = 200f;
+
     private static GameManager gameManager;
+
     private Rigidbody snowball;
+    private int currentSize;
+    private float currentExp;
 
     private void Awake()
     {
         gameManager = this;
         snowball = GameObject.Find("Snowball").GetComponent<Rigidbody>();
+        currentSize = 1;
+        currentExp = 0f;
     }
 
     // Start is called before the first frame update
@@ -33,5 +40,41 @@ public class GameManager : MonoBehaviour
     public Rigidbody GetSnowball()
     {
         return snowball;
+    }
+
+    public void GainExp(float expAmount)
+    {
+        currentExp += expAmount;
+        float expRequired = GetExpForNextSize();
+        if (currentExp > expRequired)
+        {
+            currentSize++;
+            currentExp -= expRequired;
+            snowball.transform.localScale += Vector3.one;
+        }
+    }
+
+    public void LoseExp(float expAmount)
+    {
+        currentExp -= expAmount;
+        if (currentExp < 0)
+        {
+            currentExp = 0;
+        }
+    }
+
+    public float GetExpForNextSize()
+    {
+        return currentSize * EXP_PER_SIZE_MODIFIER;
+    }
+
+    public int GetCurrentSize()
+    {
+        return currentSize;
+    }
+
+    public float GetCurrentExp()
+    {
+        return currentExp;
     }
 }
--- Assets/Scripts/SnowballController.cs
+++ Assets/Scripts/SnowballController.cs
@@ -7,14 +7,15 @@ public class SnowballController : MonoBehaviour
     private const float BASE_MOVE_SPEED = 50f;
     private const float MAX_HORIZONTAL_VELOCITY = 20f;
     private const float MOVE_STEP = 0.2f;
+    private const float EXP_GAIN_PER_SECOND = 20f;
 
     private new Rigidbody rigidbody;
     private float horizontalVelocity;
     private Vector2 touchInitialPosition;
     private RaycastHit hit;
     private int eatingAmount;
+    private GameManager gameManager;
 
-    // Start is called before the first frame update
     void Start()
     {
         eatingAmount = 0;
@@ -22,9 +23,9 @@ public class SnowballController : MonoBehaviour
         touchInitialPosition = Vector2.zero;
         rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(Vector3.forward * BASE_MOVE_SPEED + Physics.gravity * rigidbody.mass, ForceMode.Impulse);
+        gameManager = GameManager.GetGameManager();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (ShouldMove())
@@ -43,10 +44,9 @@ public class SnowballController : MonoBehaviour
         {
             rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, Mathf.Lerp(rigidbody.velocity.z, BASE_MOVE_SPEED, MOVE_STEP * 0.1f));
         }
-        if (rigidbody.velocity.z > BASE_MOVE_SPEED - 5f && Physics.Raycast(transform.position, Vector3.down, out hit, transform.localScale.y, 1 << LayerMask.NameToLayer("Ground")))
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, transform.localScale.y, 1 << LayerMask.NameToLayer("Ground")))
         {
-            ChangeScale(Time.fixedDeltaTime / (4f / (eatingAmount + 1)));
-            //rigidbody.mass = transform.localScale.y;
+            gameManager.GainExp(Time.fixedDeltaTime * EXP_GAIN_PER_SECOND);
         }
         rigidbody.velocity = new Vector3(Mathf.Lerp(rigidbody.velocity.x, horizontalVelocity, MOVE_STEP), rigidbody.velocity.y, rigidbody.velocity.z);
     }
@@ -89,22 +89,5 @@ public class SnowballController : MonoBehaviour
         float maxDistance = Screen.width * 0.25f;
         float distance = Mathf.Clamp(position.x - touchInitialPosition.x, -maxDistance, maxDistance);
         return (distance / maxDistance) * MAX_HORIZONTAL_VELOCITY;
-    }
-
-    public IEnumerator EatObstacle()
-    {
-        eatingAmount++;
-        yield return new WaitForSeconds(Obstacle.SECONDS_TO_GET_EATEN);
-        eatingAmount--;
-    }
-
-    public void ChangeScale(float changeAmount)
-    {
-        Vector3 newScale = transform.localScale + Vector3.one * changeAmount;
-        if (newScale.y < 1f)
-        {
-            newScale = Vector3.one;
-        }
-        transform.localScale = newScale;
     }
 }
